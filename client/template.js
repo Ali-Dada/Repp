47a0e86f7e306161be6dde8aa3aec90c2810366c
Ali-Dada <alidada@yahoo.com>
2017-07-15 17:10:56 +0300

    Amrija-2017-07-13-a

--- client/template.js
+++ client/template.js
@@ -1,18 +1,4 @@
 //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-/*
-Template.myForm.events({
-  'change .myFileInput': function(event, template) {
-    var files = event.target.files;
-	console.log(files);
-    for (var i = 0, ln = files.length; i < ln; i++) {
-	  console.log(i,ln,files[i]);
-      Images.insert(files[i], function (err, fileObj) {
-        //Inserted new doc with ID fileObj._id,
-		// and kicked off the data upload using HTTP
-      });
-    }
-  }
-});
 
-*/
+
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
